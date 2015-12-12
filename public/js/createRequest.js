7496bc048c97d71125cf7ba69f05e43a4ed621cd
Nabai Habtemariam <nabaihabtemariam@nat-oitwireless-inside-vapornet100-c-3355.Princeton.EDU>
2015-12-12 14:27:53 -0500

    edit to createRequest

--- public/js/createRequest.js
+++ public/js/createRequest.js
@@ -6,7 +6,15 @@ function createRequest() {
         location : $('#selectLocation').val(),
         notes : $('#otherNotes').val(),
     }
-
+    if (data.name == "") {
+        alert("Enter Name!")
+        }
+    if (data.netID == "") {
+        alert("Enter NetID!")
+        }
+    if (data.location == "") {
+        alert("Enter Location!")
+        }
 	//TODO show loading UI
 
     $.ajax({
